1c03d76c54381550c77911c95d5767bea0a818ef
AdiTya <adi31114@gmail.com>
2024-11-19 22:05:20 +0530

    first commit

---
+++ src/Component/StringCalculator.js
@@ -0,0 +1,47 @@
+import { TextField } from '@material-ui/core';
+import { Button } from '@mui/material';
+import React, { useState } from 'react';
+
+
+export default function NewComp() {
+ const [inputVal, setInputVal] = useState("")
+
+ const handleChange = (e) => { // console.log(e.target.value); 
+    setInputVal(e.target.value) 
+}
+ 
+    const handleClck = () => {
+         console.log(inputVal); 
+         if (inputVal) { 
+            const arr = inputVal.split(/[\n,]/)
+             let res = add(arr)
+              console.log(res);
+             } else { 
+                console.log("No Input To Calculate");
+             } }
+ 
+    const add = (arr) => {
+         let total = 0
+          arr.map(val => {
+             if (!isNaN(val)) { 
+                try { 
+                    if (Number(val) < 0) {
+                         throw new Error(`Negative numbers are not allowed. ${val}`) 
+                        } 
+                        total += Number(val)
+                     } catch (error) { 
+                        console.error("Error:", error.message);
+                     }
+ } else {
+     console.log(val, "is not a number"); 
+    } })
+     return total }
+ return (
+     <div>
+         <TextField type='text' onChange={handleChange} value={inputVal} sx={{ margin: "2px", }} label={"user input"} /> 
+         <Button variant='outlined' onClick={handleClck}>OnClk</Button>
+          </div> 
+          )
+        }
+
+
