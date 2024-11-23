c168429e890531b7cf60d9e1f81a47650fbaa9db
prajyot herwade <adi31114@gmail.com>
2024-11-24 00:00:11 +0530

    final touchup with adding delimiter seperation.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
-import NewComp from './Component/StringCalculator';
+import logo from "./logo.svg";
+import "./App.css";
+import NewComp from "./Component/StringCalculator";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <NewComp />
-      </header>
+      {/* <header className="App-header"> */}
+      <NewComp />
+      {/* </header> */}
     </div>
   );
 }
--- src/Component/StringCalculator.js
+++ src/Component/StringCalculator.js
@@ -1,57 +1,141 @@
-
-
-import React, { useState } from 'react';
-
+import React, { useEffect, useState } from "react";
 
 export default function NewComp() {
- const [inputVal, setInputVal] = useState("")
- const [addition, setAddition] = useState(0)
+  const [inputVal, setInputVal] = useState("");
+  const [addition, setAddition] = useState(0);
+  const [err, setErr] = useState(false);
+  const [errExp, setErrExp] = useState(false);
+  const [errNAN, setErrNAN] = useState(false);
+  const [negArr, setNegArr] = useState([]);
+  const [errMsg, setErrMsg] = useState("");
 
- const handleChange = (e) => { // console.log(e.target.value); 
-    setInputVal(e.target.value) 
-}
- 
-    const handleClck = () => {
-         console.log(inputVal,typeof(inputVal)); 
-         if (inputVal) { 
-            const arr = inputVal.split(/[\\n,]/);
-            console.log(arr);
-             let res = add(arr)
-             setAddition(res)
-              console.log(res);
-             } else { 
-                console.log("No Input To Calculate");
-             } }
- 
-    const add = (arr) => {
-         let total = 0
-          arr.map(val => {
-             if (!isNaN(val)) { 
-                try { 
-                    if (Number(val) < 0) {
-                         throw new Error(`Negative numbers are not allowed. ${val}`) 
-                        } 
-                        total += Number(val)
-                     } catch (error) { 
-                        console.error("Error:", error.message);
-                     }
- } else {
-     console.log(val, "is not a number"); 
-    } })
-     return total }
- return (
-     <div>
-         <input type='text' onChange={handleChange} value={inputVal}  sx={{ margin: "2px", }} label={"user input"} /> 
-         <button variant='outlined' onClick={handleClck}>Add</button>
-         <button variant='outlined' onClick={()=>setInputVal("")}>Clear</button>
-         <div>
-            Total Addition of input fields number is {addition}
-         </div>
-          </div> 
-          )
+  const handleChange = (e) => {
+    // console.log(e.target.value);
+    setInputVal(e.target.value);
+  };
+
+  const handleClck = () => {
+    setErrNAN(false);
+
+    setErr(false);
+
+    //  console.log(inputVal, typeof inputVal);
+    if (inputVal[0] == "/") {
+      setErrExp(true);
+      let char = inputVal[3];
+
+      const arr = inputVal.split(/[\\n]/);
+      const brr =
+        arr[2]?.split(inputVal.charAt(2)) || arr[1]?.split(inputVal.charAt(2));
+      console.log(brr);
+      console.log(arr, "asd");
+      let res = add(brr);
+      setAddition(res);
+    } else if (inputVal) {
+      setErrExp(false);
+      const arr = inputVal.split(/[\\n,]/);
+      let res = add(arr);
+      setAddition(res);
+    } else {
+      setErrExp(false);
+      console.log("No Input To Calculate");
+    }
+  };
+
+  const add = (arr) => {
+    let total = 0;
+    const brr = [];
+    console.log(arr);
+    arr?.map((val) => {
+      if (!isNaN(val)) {
+        try {
+          if (Number(val) < 0) {
+            throw new Error(`Negative numbers are not allowed. ${val}`);
+          }
+          total += Number(val);
+        } catch (error) {
+          setErr(true);
+          brr.push(val);
+          console.error("Error:", error.message);
         }
+      } else {
+        setErrNAN(true);
+        console.log(val, "is not a number");
+      }
+    });
 
+    setNegArr(brr);
+    return total;
+  };
 
-
-
-        
+  useEffect(() => {
+    if (negArr.length > 0) {
+      let res = "";
+      negArr.map((val) => {
+        res += val + ", ";
+      });
+      res += "are negative numbers. ";
+      console.log(res);
+      setErrMsg(res);
+    }
+  }, [negArr]);
+  return (
+    <div className="String">
+      <div>
+        <input
+          type="text"
+          onChange={handleChange}
+          value={inputVal}
+          style={{
+            margin: "2px",
+            width: "300px",
+            height: "35px",
+            padding: "5px",
+            borderRadius: "8px",
+          }}
+          placeholder="Input Numbers ',' seperated."
+          label={"user input"}
+        />
+        <br></br>
+        <button className="btn" variant="outlined" onClick={handleClck}>
+          Add
+        </button>
+        <button
+          className="btn"
+          variant="outlined"
+          onClick={() => {
+            setAddition(0);
+            setErr(false);
+            setErrExp(false);
+            setErrNAN(false);
+            setNegArr([]);
+            setInputVal("");
+          }}
+        >
+          Clear
+        </button>
+      </div>
+      <div className="userMsg">
+        <div style={{ marginTop: "10px" }}>
+          Total Addition of input fields number is {addition}.
+        </div>
+        {err && (
+          <div style={{ fontSize: "19px", backgroundColor: "#c8cee3a6" }}>
+            <p>Exception found with Negative Numbers, </p>
+            {errMsg}
+          </div>
+        )}
+        {errExp && (
+          <p style={{ fontSize: "14px", backgroundColor: "#c8cee3a6" }}>
+            * String found with special delimiter.
+          </p>
+        )}
+        {errNAN && (
+          <p style={{ fontSize: "14px", backgroundColor: "#c8cee3a6" }}>
+            * Input fields String contains not a number Value.
+          </p>
+        )}
+      </div>
+    </div>
+  );
+}
